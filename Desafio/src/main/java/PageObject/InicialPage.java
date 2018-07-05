7dd9f0fefb99a7bf70b57fd225bafc9f32308a20
Matheus <matheus.inhaia2014@gmail.com>
2018-07-05 03:32:20 -0300

    atualiazações

--- Desafio/src/main/java/PageObject/InicialPage.java
+++ Desafio/src/main/java/PageObject/InicialPage.java
@@ -1,5 +1,31 @@
 package PageObject;
 
-public class InicialPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+
+
+public class InicialPage {
+	
+	public WebDriver driver;
+	public InicialPageXpath inicalPageXpath; 
+	
+	public InicialPage(WebDriver driver) {
+	   this.driver = driver;
+	   this.inicalPageXpath = new InicialPageXpath();
+	 }
+	  
+	
+	 public WebElement womenBotao() {
+		return driver.findElement(By.xpath(inicalPageXpath.womenBotao));	
+	 }
+	 
+	 public WebElement dressesBotao() {
+		return driver.findElement(By.xpath(inicalPageXpath.dressesBotao));	
+	 }
+	 
+	 public WebElement tshirtsBotao() {
+		return driver.findElement(By.xpath(inicalPageXpath.tshirtsBotao));	
+	 }
 }
---
+++ Desafio/src/main/java/PageObject/InicialPageXpath.java
@@ -0,0 +1,8 @@
+package PageObject;
+
+public class InicialPageXpath {
+
+	public String womenBotao = "//*[@id=\"block_top_menu\"]/ul/li[1]/a";
+	public String dressesBotao = "//*[@id=\"block_top_menu\"]/ul/li[2]/a";
+	public String tshirtsBotao = "//*[@id=\"block_top_menu\"]/ul/li[3]/a";
+}
