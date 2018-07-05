530c98b6524157fbb622573c7b536dc4283d8a00
Matheus <matheus.inhaia2014@gmail.com>
2018-07-05 16:59:48 -0300

    mudanças

--- Desafio/src/main/java/PageObject/InicialPage.java
+++ Desafio/src/main/java/PageObject/InicialPage.java
@@ -8,24 +8,28 @@ import org.openqa.selenium.WebElement;
 
 public class InicialPage {
 	
-	public WebDriver driver;
-	public InicialPageXpath inicalPageXpath; 
+	public WebDriver driver; 
+	
+	
+	String womenBotao = "//*[@id=\"block_top_menu\"]/ul/li[1]/a";
+	String dressesBotao = "//*[@id=\"block_top_menu\"]/ul/li[2]/a";
+	String tshirtsBotao = "//*[@id=\"block_top_menu\"]/ul/li[3]/a";
+	
+	
 	
 	public InicialPage(WebDriver driver) {
 	   this.driver = driver;
-	   this.inicalPageXpath = new InicialPageXpath();
 	 }
 	  
-	
 	 public WebElement womenBotao() {
-		return driver.findElement(By.xpath(inicalPageXpath.womenBotao));	
+		return driver.findElement(By.xpath(womenBotao));	
 	 }
 	 
 	 public WebElement dressesBotao() {
-		return driver.findElement(By.xpath(inicalPageXpath.dressesBotao));	
+		return driver.findElement(By.xpath(dressesBotao));	
 	 }
 	 
 	 public WebElement tshirtsBotao() {
-		return driver.findElement(By.xpath(inicalPageXpath.tshirtsBotao));	
+		return driver.findElement(By.xpath(tshirtsBotao));	
 	 }
 }
--- Desafio/src/main/java/PageObject/InicialPageXpath.java
+++
@@ -1,8 +0,0 @@
-package PageObject;
-
-public class InicialPageXpath {
-
-	public String womenBotao = "//*[@id=\"block_top_menu\"]/ul/li[1]/a";
-	public String dressesBotao = "//*[@id=\"block_top_menu\"]/ul/li[2]/a";
-	public String tshirtsBotao = "//*[@id=\"block_top_menu\"]/ul/li[3]/a";
-}
