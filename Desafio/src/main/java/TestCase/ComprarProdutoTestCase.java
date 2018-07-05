4dbf49ecbe93b92b038020d447935fbc2707e192
Matheus <matheus.inhaia2014@gmail.com>
2018-07-05 20:53:49 -0300

    iniciando o teste

---
+++ Desafio/src/main/java/TestCase/ComprarProdutoTestCase.java
@@ -0,0 +1,14 @@
+package TestCase;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+public class ComprarProdutoTestCase {
+
+	public WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		
+	}
+}
