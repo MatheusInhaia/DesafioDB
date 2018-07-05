921f40abfed399656f9c1fe83d8e46ab434cc82c
Matheus <matheus.inhaia2014@gmail.com>
2018-07-05 18:15:49 -0300

    SummaryPage

---
+++ Desafio/src/main/java/PageObject/SummaryPage.java
@@ -0,0 +1,21 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SummaryPage {
+	
+	public WebDriver driver; 
+	
+	String proceedToCheckoutBotao = "//*[@id=\"center_column\"]/p[2]/a[1]";
+	
+	
+	public SummaryPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public WebElement proceedToCheckoutBotao() {
+		return driver.findElement(By.xpath(proceedToCheckoutBotao));
+	}
+}
