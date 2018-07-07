9eccda1aeb3da3506447bc413738c85b4cda3b01
Matheus <matheus.inhaia2014@gmail.com>
2018-07-07 03:11:52 -0300

    Criando a pagina Address

---
+++ Desafio/src/main/java/PageObject/AddressPage.java
@@ -0,0 +1,21 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class AddressPage {
+	
+	public WebDriver driver;
+	
+	String proceedToCheckout = "//*[@id=\"center_column\"]/form/p/button";
+
+
+	public AddressPage(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public WebElement proceedToCheckout() {
+		return driver.findElement(By.xpath(proceedToCheckout));
+	}
+}
